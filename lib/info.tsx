c0e7f356e2a510e7c15431c37a59ecffb3a25e7b
Gabe <28gabe@gmail.com>
2023-04-21 13:39:00 -0700

    edits info

--- lib/info.tsx
+++ lib/info.tsx
@@ -5,8 +5,9 @@ export const avatar = me;
 export const about = () => {
 	return (
 		<>
-			Hi, I&apos;m Gabe. I&apos;m a Frontend Engineer from San Diego, CA.
-			I build interfaces that optimize both form and function.
+			Hi, I&apos;m Gabe. I&apos;m a{' '}
+			<b>Frontend Engineer from San Diego, CA</b>. I build interfaces that
+			optimize both form and function.
 		</>
 	);
 };
