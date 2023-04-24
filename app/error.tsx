8ec4797e6a0f466f233cfd9cf5bac3134bd19a8c
Gabe <28gabe@gmail.com>
2023-04-24 07:40:19 -0700

    adds portfolio into list of projects

--- app/error.tsx
+++ app/error.tsx
@@ -16,7 +16,7 @@ export default function Error({
 
 	return (
 		<div>
-			<p>Oh no, something went wrong... maybe refresh?</p>
+			<p>Well this is embarassing... maybe refresh?</p>
 		</div>
 	);
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,28 +3,47 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Page from '../../components/Page';
+import macrolatorGif from '../../public/macrolator.gif';
+import portfolioGif from '../../public/porfolio.gif';
 
-const ProjectsPage = () => {
+const Project = ({ link, name, gifPath }) => {
 	return (
-		<Page title='Projects'>
+		<>
 			<h2 className='text-2xl font-mono text-zinc-800 dark:text-zinc-300 my-5'>
-				The Macrolator
+				{name}
 			</h2>
 			<div className='relative group'>
 				<Link
 					className='absolute inset-0 bg-zinc-900 opacity-0 group-hover:opacity-70 transition flex justify-center items-center'
-					href='https://github.com/gabe-santos/calorie-macro-calc'>
+					href={link}>
 					<span className='text-white text-lg'>Github</span>
 				</Link>
 				<Image
 					alt='macrolator gif'
 					className=''
-					src={require('../../public/macrolator.gif')}
+					src={gifPath}
 					height={100}
 					width={800}
 					priority
 				/>
 			</div>
+		</>
+	);
+};
+
+const ProjectsPage = () => {
+	return (
+		<Page title='Projects'>
+			<Project
+				link='https://github.com/gabe-santos/calorie-macro-calc'
+				name='The Macrolator'
+				gifPath={macrolatorGif}
+			/>
+			<Project
+				link='https://github.com/gabe-santos/gabe-santos-portfolio'
+				name='Porfolio Website'
+				gifPath={portfolioGif}
+			/>
 		</Page>
 	);
 };
