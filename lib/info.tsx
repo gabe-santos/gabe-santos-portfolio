8c41aeb55fe54f2b44cd0ae03f00b67cacf41368
Gabe <28gabe@gmail.com>
2023-04-24 04:28:25 -0700

    updates title

--- lib/info.tsx
+++ lib/info.tsx
@@ -3,17 +3,17 @@ import me from '../public/avatar.jpg';
 export const name = 'Gabe Santos';
 export const avatar = me;
 export const about = () => {
-	return (
-		<>
-			Hi, I&apos;m Gabe. I&apos;m a{' '}
-			<span className='font-mono font-bold'>
-				Frontend Engineer from San Diego, CA
-			</span>
-			. I build interfaces that optimize both form and function.
-		</>
-	);
+  return (
+    <>
+      Hi, I&apos;m Gabe. I&apos;m a{' '}
+      <span className="font-mono font-bold">
+        Frontend Developer from San Diego, CA
+      </span>
+      . I build interfaces that optimize both form and function.
+    </>
+  );
 };
 
 export const bio = () => {
-	return <>Let&apos;s build something awesome.</>;
+  return <>Let&apos;s build something awesome.</>;
 };
