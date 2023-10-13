98c87c6c69054ad762c4009974f22b15f7f417d8
Gabe <28gabe@gmail.com>
2023-10-12 20:11:53 -0700

    adds prettier tailwind plugin

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,50 +8,46 @@ import macrolatorGif from '../../public/macrolator.gif';
 import portfolioGif from '../../public/porfolio.gif';
 
 const Project = ({ link, name, gifPath }) => {
-	return (
-		<>
-			<h2 className='text-2xl font-mono text-zinc-800 dark:text-zinc-300 my-5'>
-				{name}
-			</h2>
-			<div className='relative group'>
-				<Link
-					className='absolute inset-0 bg-zinc-900 opacity-0 group-hover:opacity-70 transition flex justify-center items-center'
-					href={link}>
-					<span className='text-white text-lg'>Github</span>
-				</Link>
-				<Image
-					alt='macrolator gif'
-					className=''
-					src={gifPath}
-					height={100}
-					width={800}
-					priority
-				/>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <h2 className='my-5 font-mono text-2xl text-zinc-800 dark:text-zinc-300'>
+        {name}
+      </h2>
+      <div className='group relative'>
+        <Link
+          className='absolute inset-0 flex items-center justify-center bg-zinc-900 opacity-0 transition group-hover:opacity-70'
+          href={link}
+        >
+          <span className='text-lg text-white'>Github</span>
+        </Link>
+        <Image
+          alt='macrolator gif'
+          className=''
+          src={gifPath}
+          height={100}
+          width={800}
+          priority
+        />
+      </div>
+    </>
+  );
 };
 
 const ProjectsPage = () => {
-	return (
-		<Page title='Projects'>
-			<Project
-				link='https://github.com/gabe-santos/excursionist'
-				name='Excursionist'
-				gifPath={excursionistGif}
-			/>
-			<Project
-				link='https://github.com/gabe-santos/calorie-macro-calc'
-				name='The Macrolator'
-				gifPath={macrolatorGif}
-			/>
-			<Project
-				link='https://github.com/gabe-santos/gabe-santos-portfolio'
-				name='Porfolio Website'
-				gifPath={portfolioGif}
-			/>
-		</Page>
-	);
+  return (
+    <Page title='Projects'>
+      <Project
+        link='https://github.com/gabe-santos/excursionist'
+        name='Excursionist'
+        gifPath={excursionistGif}
+      />
+      <Project
+        link='https://github.com/gabe-santos/calorie-macro-calc'
+        name='The Macrolator'
+        gifPath={macrolatorGif}
+      />
+    </Page>
+  );
 };
 
 export default ProjectsPage;
